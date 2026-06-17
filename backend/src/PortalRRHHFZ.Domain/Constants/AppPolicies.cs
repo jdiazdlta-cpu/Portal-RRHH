9b6990785f6ba38078aa19fd68d73f16070fef42
Diaz <j.diaz@farmazona.com>
2026-06-17 11:51:00 -0500

    Actualizar V1 estable Portal RRHH FZ

--- backend/src/PortalRRHHFZ.Domain/Constants/AppPolicies.cs
+++
@@ -1,9 +0,0 @@
-namespace PortalRRHHFZ.Domain.Constants;
-
-public static class AppPolicies
-{
-    public const string RequireAdmin = "RequireAdmin";
-    public const string RequireAdminOrRRHH = "RequireAdminOrRRHH";
-    public const string RequireSupervisor = "RequireSupervisor";
-    public const string RequireConsulta = "RequireConsulta";
-}
--- backend/src/PortalRRHHFZ.Domain/Constants/AppRoles.cs
+++
@@ -1,9 +0,0 @@
-namespace PortalRRHHFZ.Domain.Constants;
-
-public static class AppRoles
-{
-    public const string Admin = "Admin";
-    public const string RRHH = "RRHH";
-    public const string Supervisor = "Supervisor";
-    public const string Consulta = "Consulta";
-}
